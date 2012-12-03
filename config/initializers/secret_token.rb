f969b7deee4bcfb5c9778e1250d23837ddee7287
IS Lab 1601 <islab01@dyn-86-36-42-103.wv.qatar.cmu.edu>
2012-12-03 14:38:56 +0300

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FUNN::Application.config.secret_token = '2386c3856545b4d57549d823fa4e0e1e3e2c70f545a5037543b4d2571619f05e18a4bc32c83dfa6ca218b56bcb31e397a03511a9dc4354a68b2567a0deb86a00'
